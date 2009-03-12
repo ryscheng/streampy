a2c73c2cc8114dfae02095678132c182147fab97
Matthew Peddie <peddie@dudochnik.MIT.EDU>
2009-03-11 23:19:43 -0400

    load.py with more comments

--- load.py
+++ load.py
@@ -5,14 +5,20 @@
 import urllib
 import sys
 
-urllib.urlcleanup()
+# Eventually you'll get these from apt-run.  
 firstsegurl = "http://code.general-rotors.com/streambin/streamtest.pyc"
 firstsegname = "testdown"
 firstsegpath = "/tmp/streambin/"
 
+# This is so that you can load the new modules.
 sys.path.append(firstsegpath)
 
 (firstseg, headers) = urllib.urlretrieve(firstsegurl, firstsegpath+firstsegname+".pyc");
 
 __import__(firstsegname)
 
+# This is here as a test -- I want to find a way to get around the common idiom
+# if __name__ == '__main__':
+#    do_more_stuff()
+
+#exec "import " + firstsegname + " as __main__"
