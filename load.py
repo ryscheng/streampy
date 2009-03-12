2ead76a59b8ed6a7917d6e251ac18442d12655c4
Matthew Peddie <matt@general-rotors.com>
2009-03-12 00:49:28 -0400

    now with recursive import following

--- load.py
+++ load.py
@@ -4,19 +4,36 @@
 
 import urllib
 import sys
+import threading
 
 # Eventually you'll get these from apt-run.  
-firstsegurl = "http://code.general-rotors.com/streambin/streamtest.pyc"
-firstsegname = "testdown"
+rooturl = "http://code.general-rotors.com/streambin/"
+firstsegurl = "streamtest.pyc"
+firstsegname = "streamtest"
 firstsegpath = "/tmp/streambin/"
 
 # This is so that you can load the new modules.
 sys.path.append(firstsegpath)
 
-(firstseg, headers) = urllib.urlretrieve(firstsegurl, firstsegpath+firstsegname+".pyc");
 
-__import__(firstsegname)
 
+def erroryank(error):
+    return error.message[16:]
+
+def streamrun(segname):
+    (seg, headers) = urllib.urlretrieve(rooturl+segname+".pyc", firstsegpath+segname+".pyc");
+    try:
+        __import__(segname)
+    except ImportError, (err):
+        name = erroryank(err)
+        if name == segname:
+            raise
+        else:
+            streamrun(name)
+            streamrun(segname)
+
+streamrun(firstsegname)
+    
 # This is here as a test -- I want to find a way to get around the common idiom
 # if __name__ == '__main__':
 #    do_more_stuff()
